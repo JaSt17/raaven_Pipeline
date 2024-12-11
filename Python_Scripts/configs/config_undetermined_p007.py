8a9487ea44e1b64df61bfa2179845bbdfc1a5ab6
Jaro Steindorff <jarost@cosmos2.lunarc.lu.se>
2024-12-11 14:08:27 +0100

    Small logger changes to S4; added undetermined configs and slurm script

---
+++ Python_Scripts/configs/config_undetermined_p007.py
@@ -0,0 +1,65 @@
+""" This file contains the config dictionary that are used to store the configuration parameters for the pipeline. """
+
+# Define the data directory where the input and output files are stored
+data_dir = "raav-60/undetermined"
+# Define the directory where the logs are stored
+log_dir = data_dir + "/logs/"
+
+config_S2 = {
+    # input file names for the P5 and P7 fastq files P5 is the barcode and P7 is the fragment
+    "in_name_barcode": data_dir + "/fastq_files/Undetermined_R1.fastq.gz",
+    "in_name_fragment": data_dir + "/fastq_files/Undetermined_R2.fastq.gz",
+    # output directory and name for the barcode and fragment files once they have been extracted
+    "out_dir": data_dir + "/barcode_fragment",
+    "out_name": "undetermined_p007",
+    # arguments for the bbduk2 tool to extract the barcode and fragment sequences
+    "bbduk2_args_BC" : [
+        "k=20",
+        "hammingdistance=2",
+        "overwrite=true",
+        "findbestmatch=t",
+        "rcomp=f",
+        "qhdist=1",
+        "minavgquality=0",
+        "maxns=0",
+        "minlength=27",
+        "maxlength=27",
+        "ordered=t",
+        "lliteral=GGCTTGTGTCTATCGCAAGA",
+        "rliteral=ATAACTTCGTATAATGTATGC",
+    ],
+    "bbduk2_args_Frag" : [
+        "k=18",
+        "hammingdistance=2",
+        "overwrite=true",
+        "findbestmatch=t",
+        "maskmiddle=t",
+        "rcomp=f",
+        "qhdist=1",
+        "minavgquality=0",
+        "maxns=0",
+        "minlength=25",
+        "maxlength=25",
+        "ordered=t",
+        "lliteral=ACCAACCTCCAGAGAGGCAACG",
+        "rliteral=CAGACAAGCAGCTACCGCAGAT",
+    ],
+    "log_dir": log_dir,
+}
+
+# create a lookup dictionary for the configuration dictionaries
+config_lookup = {
+    "S2": config_S2,
+}
+
+def get_config(step: str) -> dict:
+    """
+    Returns the configuration dictionary for the specified step.
+
+    :param step: The step for which to return the configuration dictionary
+    :return: The configuration dictionary for the specified step
+    """
+    try:
+        return config_lookup[step]
+    except KeyError:
+        raise ValueError(f"Step {step} not found in the configuration lookup dictionary")
---
+++ Slurm_Scripts/undetermined.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH --job-name=undetermined_analysis
+#SBATCH --nodes=12
+#SBATCH --ntasks=12
+#SBATCH --cpus-per-task=48
+#SBATCH --time=48:00:00
+#SBATCH -A lu2024-2-8
+#SBATCH -o undetermined_analysis%j.out
+#SBATCH -e undetermined_analysis%j.err
+
+# exit when any command fails
+set -e
+
+module load Anaconda3/2024.02-1
+
+# activate the conda environment
+source config_conda.sh
+conda activate python_pipeline
+
+# for p005
+# change the config filte to undetermined_p005.py
+cp Python_Scripts/configs/config_undetermined_p005.py Python_Scripts/config.py
+# run the second step of the pipeline
+./Python_Scripts/S2.py
+# rename the S2.log file to S2_undetermined_p005.log
+mv raav-60/undetermined/logs/S2.log raav-60/undetermined/logs/S2_undetermined_p005.log
+
+# for p006
+# change the config filte to undetermined_p006.py
+cp Python_Scripts/configs/config_undetermined_p006.py Python_Scripts/config.py
+# run the second step of the pipeline
+./Python_Scripts/S2.py
+# rename the S2.log file to S2_undetermined_p006.log
+mv raav-60/undetermined/logs/S2.log raav-60/undetermined/logs/S2_undetermined_p006.log
+
+# for p007
+# change the config filte to undetermined_p007.py
+cp Python_Scripts/configs/config_undetermined_p007.py Python_Scripts/config.py
+# run the second step of the pipeline
+./Python_Scripts/S2.py
+# rename the S2.log file to S2_undetermined_p007.log
+mv raav-60/undetermined/logs/S2.log raav-60/undetermined/logs/S2_undetermined_p007.log
+
