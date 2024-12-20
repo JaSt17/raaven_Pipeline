4a4e672515bba197f0c3868303c1affa61f9453f
Jaro Steindorff <jarost@cosmos3.lunarc.lu.se>
2024-12-20 09:51:54 +0100

    Adjusted Slurm Scripts

--- Slurm_Scripts/run_all.sh
+++ Slurm_Scripts/run_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=all_analysis
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
+#SBATCH --nodes=4
+#SBATCH --ntasks=4
 #SBATCH --cpus-per-task=48
 #SBATCH --time=96:00:00
 #SBATCH -A lu2024-2-8
@@ -16,15 +16,6 @@ module load Anaconda3/2024.02-1
 # activate the conda environment
 source config_conda.sh
 conda activate python_pipeline
-
-# change the config to the BRAVE config and rerun the BRAVE pipeline
-cp Python_Scripts/configs/config_BRAVE.py Python_Scripts/config.py
-#./Python_Scripts/S1.py
-#./Python_Scripts/S2.py
-./Python_Scripts/S3.py
-#./Python_Scripts/S4.py
-./Python_Scripts/S5.py
-./Python_Scripts/S6.py
 
 # change the config file to p005
 cp Python_Scripts/configs/config_p005.py Python_Scripts/config.py
@@ -44,8 +35,17 @@ cp Python_Scripts/configs/config_p006.py Python_Scripts/config.py
 
 # change the config file to p007
 cp Python_Scripts/configs/config_p007.py Python_Scripts/config.py
-#./Python_Scripts/S1.py
+./Python_Scripts/S1.py
 #./Python_Scripts/S2.py
 ./Python_Scripts/S3.py
 ./Python_Scripts/S4.py
 ./Python_Scripts/S5.py
+
+# change the config to the BRAVE config and rerun the BRAVE pipeline
+cp Python_Scripts/configs/config_BRAVE.py Python_Scripts/config.py
+./Python_Scripts/S1.py
+#./Python_Scripts/S2.py
+./Python_Scripts/S3.py
+./Python_Scripts/S4.py
+./Python_Scripts/S5.py
+./Python_Scripts/S6.py
