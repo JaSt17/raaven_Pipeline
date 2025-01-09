2aa84d1fce2ec607620b82b3b062baa010b60ef9
Jaro Steindorff <jarost@cosmos3.lunarc.lu.se>
2025-01-09 13:30:00 +0100

    Rerun

--- Slurm_Scripts/p006.sh
+++ Slurm_Scripts/p006.sh
@@ -21,7 +21,7 @@ conda activate python_pipeline
 cp configs/config_p006.py Python_Scripts/config.py
 #running only S2 and S3_NNK since p006 does not have reference data
 #./Python_Scripts/S2_NNK.py
-#./Python_Scripts/S3_NNK.py
+./Python_Scripts/S3_NNK.py
 ./Python_Scripts/S4.py
-#./Python_Scripts/S5.py
+./Python_Scripts/S5.py
 #./Python_Scripts/S6.py
