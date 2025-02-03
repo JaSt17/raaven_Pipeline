8cdd6f23bc7ea2e95ef3d44a691836b0910e0528
Jaro Steindorff <jarost@cosmos1.int.lunarc>
2025-02-03 17:30:47 +0100

    Updated Brave pipeline to the new scripts

--- Slurm_Scripts/Brave.sh
+++ Slurm_Scripts/Brave.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=brave_analysis
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
+#SBATCH --nodes=3
+#SBATCH --ntasks=3
 #SBATCH --cpus-per-task=48
 #SBATCH --time=24:00:00
 #SBATCH -A lu2024-2-8
@@ -20,9 +20,13 @@ conda activate python_pipeline
 # change the config to the BRAVE config and rerun the BRAVE pipeline
 cp configs/config_BRAVE.py Python_Scripts/config.py
 ./Python_Scripts/S1.py
-# we are using the NNK S2 script for BRAVE since we are looking for multiple different fragment types (14aa, 22aa, etc)
-./Python_Scripts/S2_NNK.py
+cp configs/config_BRAVE.py Python_Scripts/config.py
+./Python_Scripts/S2.py
+cp configs/config_BRAVE.py Python_Scripts/config.py
 ./Python_Scripts/S3.py
+cp configs/config_BRAVE.py Python_Scripts/config.py
 ./Python_Scripts/S4.py
+cp configs/config_BRAVE.py Python_Scripts/config.py
 ./Python_Scripts/S5.py
+cp configs/config_BRAVE.py Python_Scripts/config.py
 ./Python_Scripts/S6.py
--- configs/config_BRAVE.py
+++ configs/config_BRAVE.py
@@ -4,11 +4,14 @@
 data_dir = "Brave"
 # Define the directory where the logs are stored
 log_dir = data_dir + "/logs/"
+# Define the length of the barcode and fragment sequences in DNA bases
+bc_len = 20
+frag_len = 42
 
 # configuration for Step 1 in the pipeline
 config_S1 = {
     # input file containing the DNA sequences to create the library from
-    "input_file": data_dir + "/input/DNA-lib_RetrogradeTransport.fasta",
+    "input_file": data_dir + "/input/reference_seq.fasta",
     # wSet file containing the hsa codon usage table
     "wSet": data_dir + "/input/wSet.csv",
     # dictionary containing the information about the different structures with
@@ -16,12 +19,7 @@ config_S1 = {
     "structure_dict": {
         "14aa": {"length": 14, "freq": 1,
                 "overhangs": ["AACCTCCAGAGAGGCAACGCT", "GCCAGACAAGCAGCTACCGCA"]},
-        "14aaG4S": {"length": 14, "freq": 3,
-                    "overhangs": ["AACCTCCAGAGAGGCAACGGAGGCGGAGGAAGT", "GGAGGCGGCGGAAGCAGACAAGCAGCTACCGCA"]},
-        "14aaA5": {"length": 14, "freq": 3,
-                    "overhangs": ["AACCTCCAGAGAGGCAACGCTGCTGCAGCAGCC", "GCAGCTGCAGCTGCCAGACAAGCAGCTACCGCA"]},
-        "22aa": {"length": 22, "freq": 3,
-                    "overhangs": ["AACCTCCAGAGAGGCAACGCT", "GCCAGACAAGCAGCTACCGCA"]}},
+    },
     # output file names for the LUT csv and the list of all inserted fragments
     "output_csv": data_dir + "/LUT.csv",
     "output_name": data_dir + "/SortedFragments.txt",
@@ -32,40 +30,39 @@ config_S2 = {
     # input file names for the P5 and P7 fastq files P5 is the barcode and P7 is the fragment
     "in_name_barcode": data_dir + "/fastq_files/DNA_pscAAVlib_1.fastq.gz",
     "in_name_fragment": data_dir + "/fastq_files/DNA_pscAAVlib_2.fastq.gz",
+    "input_file": config_S1["input_file"],
     # output directory and name for the barcode and fragment files once they have been extracted
     "out_dir": data_dir + "/barcode_fragment",
     "out_name": "DNA_pscAAVlib_1",
     # arguments for the bbduk2 tool to extract the barcode and fragment sequences
     "bbduk2_args_BC" : [
-        "k=18",
-        "hammingdistance=3",
+        "k=20",
+        "hammingdistance=1",
         "overwrite=true",
         "findbestmatch=t",
         "rcomp=f",
-        "qhdist=1",
         "minavgquality=0",
         "maxns=0",
-        "minlength=18",
-        "maxlength=22",
+        f"minlength={bc_len}",
+        f"maxlength={bc_len}",
         "ordered=t",
-        "lliteral=GGCCTAGCGGCCGCTTTACTT",
-        "rliteral=ATAACTTCGTATAATGTATGC",
+        "lliteral=GCCTAGCGGCCGCTTTACTT",
+        "rliteral=ATAACTTCGTATAATGTATG",
     ],
     "bbduk2_args_Frag" : [
-        "k=16",
-        "hammingdistance=3",
+        "k=20",
+        "hammingdistance=1",
         "overwrite=true",
         "findbestmatch=t",
         "maskmiddle=t",
         "rcomp=f",
-        "qhdist=1",
         "minavgquality=0",
         "maxns=0",
-        "minlength=38",
-        "maxlength=78",
+        f"minlength={frag_len}",
+        f"maxlength={frag_len}",
         "ordered=t",
-        "lliteral=ACCAACCTCCAGAGAGGCAACG",
-        "rliteral=CAGACAAGCAGCTACCGCAGAT",
+        "lliteral=ACCTCCAGAGAGGCAACGCT",
+        "rliteral=GCCAGACAAGCAGCTACCGC",
     ],
     "log_dir": log_dir,
 }
@@ -75,10 +72,12 @@ config_S3 = {
     "in_name_LUT": config_S1["output_csv"],
     "barcode_file": config_S2["out_dir"] + "/barcode_" + config_S2["out_name"] + ".fastq.gz",
     "fragment_file": config_S2["out_dir"] + "/fragment_" + config_S2["out_name"] + ".fastq.gz",
+    # Do we want to allwo single read barcodes
+    "single_read": True,
     # threshold for the ratio of the most frequent barcode to all found barcodes for chimeric barcode detection
-    "threshold": 0.6,
+    "threshold": 1,
     # the chunk size determains how many sequences are read in at once and can be set to a smaller number if memory is an issue
-    "chunk_size": 25000000,
+    "chunk_size": 10000000,
     # output file name for the library barcodes
     "out_name": data_dir + "/library_barcodes.csv",
     "log_dir": log_dir,
@@ -89,6 +88,8 @@ config_S4 = {
     "input_table": config_S3["out_name"],
     "in_name_LUT": config_S1["output_csv"],
     "chunk_size": config_S3["chunk_size"],
+    "bc_len": bc_len,
+    "db": data_dir + "/barcode_db.fasta",
     # input csv file containing the file names of all samples that should be used for barcode extraction
     "sample_inputs": data_dir + "/input/load_list.csv",
     # directory containing the fastq files for the samples
@@ -99,21 +100,19 @@ config_S4 = {
     "output_dir": data_dir + "/found_barcodes",
     # arguments for the bbduk2 tool to extract the barcodes from the samples
     "bbduk2_args" : [        
-        "k=13",
-        "hammingdistance=2",
+        "k=10",
+        "hammingdistance=1",
         "overwrite=true",
         "findbestmatch=t",
+        "maskmiddle=t",
         "rcomp=f",
-        "qhdist=1",
-        "trd=t",
-        "skipr2=t",
         "minavgquality=0",
         "maxns=0",
-        "minlength=18",
-        "maxlength=22",
+        f"minlength={bc_len}",
+        f"maxlength={bc_len}",
         "ordered=t",
-        "lliteral=GGCCTAGCGGCCGCTTTACTT",
-        "rliteral=ATAACTTCGTATA",
+        "lliteral=CGCTTTACTT",
+        "rliteral=ATAACTTCGT",
     ],
     "log_dir": log_dir,
 }
@@ -138,17 +137,9 @@ config_S6 = {
     # dictionary containing the information about the different subsets that should be created
     # the key is the name of the subset and the value is a list of the fragments that should be included
     "subsets": {
-        "Infective_AAVs": ['exclude','DNA_AAVlib_DNAse_30cpc', 'DNA_AAVlib_DNAse_3cpc','Plasmid_Library', 'DNA_pscAAVlib_Prep2'],
-        "DNAse_resistant_AAVs": ['include', 'DNA_AAVlib_DNAse_30cpc','DNA_AAVlib_DNAse_3cpc'],
+        "Infective_AAVs": ['exclude','DNA_AAVlib_DNAse_30cpc_1', 'DNA_AAVlib_DNAse_3cpc_1','Plasmid_Library', 'DNA_pscAAVlib_Prep2_1'],
+        "DNAse_resistant_AAVs": ['include', 'DNA_AAVlib_DNAse_30cpc_1','DNA_AAVlib_DNAse_3cpc_1'],
         "Transported_AAVs": ['contains_include', "mRNA_30cpc_SN", "mRNA_30cpc_Th", "mRNA_30cpc_Ctx", "mRNA_3cpc_SN", "mRNA_3cpc_Th", "mRNA_3cpc_Ctx"],
-    },
-    "backbone_seq": ["aacctccagagaggcaacg", "cagacaagcagctaccgca"],
-    # dictionary containing the information about how different structures should be trimmed
-    "trim_dict": {
-        "14aa": [2,44],
-        "14aaG4S": [14,56],
-        "14aaA5": [14,56],
-        "22aa": [2,68],
     },
     # output file name for the final fragments summary
     "output_table": data_dir + "/final_fragments_summary.csv",
